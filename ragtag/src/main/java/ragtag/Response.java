9200fb13896139c012117010ed9882f8edcc0c89
Paul Weinhold <weinpau@web.de>
2014-11-10 18:48:50 +0100

    transformed Response to interface

--- ragtag/src/main/java/ragtag/Response.java
+++ ragtag/src/main/java/ragtag/Response.java
@@ -6,83 +6,24 @@ import java.util.Optional;
  *
  * @author weinpau
  */
-public final class Response {
+public interface Response {
 
-    ResponseStatus status = ResponseStatus.OK;
-    MediaType type = MediaType.WILDCARD;
-    Object body;
+    Optional<String> body();
 
-    private Response() {
+    <T> Optional<T> body(Class<T> bodyType);
 
-    }
+    Response body(Object object);
 
-    public Optional<String> body() {
-        if (body == null) {
-            return Optional.empty();
-        } else {
-            return Optional.ofNullable(body.toString());
-        }
-    }
+    Optional<String> header(String name);
 
-    public <T> Optional<T> body(Class<T> bodyType) {
-        return Optional.empty();
-    }
+    Response header(String name, String value);
 
-    public Response body(Object object) {
-        return this;
-    }
+    Response cookie(String name, String value);
 
-    public Optional<String> header(String name) {
-        return Optional.empty();
-    }
+    Response cookie(Cookie cookie);
 
-    public Response header(String name, String value) {
-        return this;
-    }
+    MediaType type();
 
-    public Response cookie(String name, String value) {
-        return this;
-    }
-
-    public Response cookie(Cookie cookie) {
-        cookie.name();
-        return this;
-    }
-
-    public MediaType type() {
-        return type;
-    }
-
-    public Response type(MediaType type) {
-        return this;
-    }
-
-    public static Response ok() {
-        return new Response();
-    }
-
-    public static Response ok(Object body) {
-        return new Response();
-    }
-
-    public static Response created(Object body) {
-        return new Response();
-    }
-
-    public static Response notFound() {
-        return new Response();
-    }
-
-    public static Response serverError() {
-        return new Response();
-    }
-
-    public static Response redirect(String path) {
-        return new Response();
-    }
-
-    public static Response status(ResponseStatus responseStatus) {
-        return new Response();
-    }
+    Response type(MediaType type);
 
 }
